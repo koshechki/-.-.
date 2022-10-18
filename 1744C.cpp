e6026d8444f7bc94832f27569eba6a191d0c6199
koshechki <113916221+koshechki@users.noreply.github.com>
2022-10-18 15:53:40 +0300

    Add files via upload

---
+++ 1744C.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+int main() {
+    int t(0);
+    std::cin >> t;
+    for (int i(0); i < t; i++) {
+        int tek(0);
+        char start;
+        char s;
+        std ::cin >> tek;
+        std::cin >> start;
+        std::cin >> s;
+        s2 = s + s;
+        int max(0);
+        
+        
+    }
+}
